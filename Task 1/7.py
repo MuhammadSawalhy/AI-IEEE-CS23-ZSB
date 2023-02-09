7921837c6c850bd33644608f00eb479688fabc38
محمد الصوالحي <ms.2052001@gmail.com>
2023-02-09 23:45:45 +0200

    fix task 1 / problem 7

--- Task 1/7.py
+++ Task 1/7.py
@@ -2,9 +2,8 @@ nums = [int(x) for x in input().split()]
 k = int(input())
 
 n = len(nums)
-rotated = [0] * n
-for i in range(n):
-    rotated[i] = nums[i - k]
+k %= n
+rotated = nums[-k:] + nums[:n-k]
 
 print(rotated)
 
