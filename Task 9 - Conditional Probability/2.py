75b415281ac9767df23437e3789909906e4bea7f
محمد الصوالحي <ms.2052001@gmail.com>
2023-03-17 13:05:25 +0200

    udpate problem 2 in task 9
    A palindrome doesn't change when we reverse it. 😀

--- Task 9 - Conditional Probability/2.py
+++ Task 9 - Conditional Probability/2.py
@@ -1,6 +1,5 @@
-# Submission: https://leetcode.com/problems/valid-palindrome/submissions/915926217/
+# Submission: https://leetcode.com/problems/valid-palindrome/submissions/916816572/
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_s = list(filter(lambda c: c.isalnum(), s.lower()))
-        return new_s[:len(new_s)//2] == new_s[:(len(new_s) - 1)//2:-1]
-
+        return new_s == new_s[::-1]
